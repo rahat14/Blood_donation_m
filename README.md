701b4e0c4d3dad0befba4217656d8c46211dedeb
Rahatul Aseaikin Shovo <rahatshovo6666@gmail.com>
2022-03-08 11:45:13 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blood_donation_m
+ 
---
+++ app/src/main/java/com/metacoders/blood_donation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.metacoders.blood_donation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
